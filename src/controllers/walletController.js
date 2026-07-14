1b77396d2ca3e6062739bc39a37940f91f6394a2
Kehindeajiboye <ajiboyekehinde194@gmail.com>
2026-07-15 00:02:18 +0100

    Add sequelize import to walletController

--- src/controllers/walletController.js
+++ src/controllers/walletController.js
@@ -1,5 +1,6 @@
 const { v4: uuidv4 } = require("uuid");
 const { Users, Wallets, Otps, Transactions, Ledgers } = require("../../models");
+const { sequelize } = require("../../models");
 const {
   debitSenderWallet,
   creditReceiverWallet,
